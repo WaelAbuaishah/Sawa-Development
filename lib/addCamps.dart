b3454dcfc2b4ade5e0ebad555db9d6becda1c93a
WaelAbuaishahNabed <wael.abuaishah@nabed.net>
2020-03-01 23:59:02 +0200

    Layout updates

--- lib/addCamps.dart
+++ lib/addCamps.dart
@@ -1,12 +1,6 @@
-
-
-
-
-
-
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 import 'package:unhcr_camps/Services/firebase.dart';
 
@@ -14,118 +8,143 @@ import 'Models/Camps.dart';
 
 class addCamps extends StatefulWidget {
   CampsInfo campsInfo;
+
   addCamps({this.campsInfo});
+
   @override
   _addCampsState createState() => _addCampsState();
 }
 
 class _addCampsState extends State<addCamps> {
-  FireStorage fireStorage=new FireStorage();
-  TextEditingController campName= TextEditingController();
-  TextEditingController campCode= TextEditingController();
-  TextEditingController location= TextEditingController();
-  TextEditingController tentsCount= TextEditingController();
-  TextEditingController familyCounts=TextEditingController();
-  TextEditingController needs=TextEditingController();
+  FireStorage fireStorage = new FireStorage();
+  TextEditingController campName = TextEditingController();
+  TextEditingController campCode = TextEditingController();
+  TextEditingController location = TextEditingController();
+  TextEditingController tentsCount = TextEditingController();
+  TextEditingController familyCounts = TextEditingController();
+  TextEditingController needs = TextEditingController();
+
   @override
   void initState() {
-super.initState();
-if(widget.campsInfo!=null) {
-  campName.text = widget.campsInfo.campName;
-  campCode.text=widget.campsInfo.campCode;
-  tentsCount.text = widget.campsInfo.tentsCount.toString();
-  familyCounts.text = widget.campsInfo.familyCounts.toString();
-  location.text = widget.campsInfo.location;
-  needs.text = widget.campsInfo.needs;
-}
+    super.initState();
+    if (widget.campsInfo != null) {
+      campName.text = widget.campsInfo.campName;
+      campCode.text = widget.campsInfo.campCode;
+      tentsCount.text = widget.campsInfo.tentsCount.toString();
+      familyCounts.text = widget.campsInfo.familyCounts.toString();
+      location.text = widget.campsInfo.location;
+      needs.text = widget.campsInfo.needs;
+    }
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Camps Info"),
+        title: Text("معلومات المخيم"),
       ),
       body: Container(
-        padding:const EdgeInsets.all(10),
-            child: ListView(
-              children: <Widget>[
-                TextField(
-                  controller: campName,
-                  decoration: InputDecoration(
-                    labelText: "Camp Name",
-                  ),
-
-                ),
-                TextField(
-                  readOnly: (widget.campsInfo==null)?false:true,
-                  controller: campCode,
-                  decoration: InputDecoration(
-                    labelText: "camp Code ",
-                  ),
-
-                ),
-                TextField(
-                  controller: location,
-                  decoration: InputDecoration(
-                    labelText: "location",
-                  ),
-
-                ),
-                TextField(
-                  keyboardType:TextInputType.number,
-                  controller: tentsCount,
-                  decoration: InputDecoration(
-                    labelText: "tents Count",
-                  ),
-
-                ),
-                TextField(
-                  keyboardType:TextInputType.number,
-                  controller: familyCounts,
-                  decoration: InputDecoration(
-                    labelText: "family Counts",
-                  ),
-
-                ),
-                TextField(
-                  controller: needs,
-                  decoration: InputDecoration(
-                    labelText: "Needs",
-                  ),
-
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    (widget.campsInfo==null)?RaisedButton(
-                      child: Text("Add Camps"),
-                      onPressed: (){
-                        fireStorage.addCamps(
-                            CampsInfo(campName: campName.text,campCode: campCode.text,
-                                location: location.text,needs: needs.text,
-                                tentsCount:int.parse( tentsCount.text),familyCounts: int.parse(familyCounts.text)));
-                        Navigator.pop(context);
-                        },
-                    ):RaisedButton(
-                      child: Text("update Camps"),
-                      onPressed: (){
-                        fireStorage.updateCamps(
-                            CampsInfo(campName: campName.text,campCode: campCode.text,
-                                location: location.text,needs: needs.text,
-                                tentsCount:int.parse( tentsCount.text),familyCounts: int.parse(familyCounts.text)));
-                        Navigator.pop(context);
-                      },
-                    ),
-                  ],
-                )
-
-              ],
-
-
+        padding: const EdgeInsets.all(10),
+        child: ListView(
+          children: <Widget>[
+            TextField(
+              controller: campName,
+              decoration: InputDecoration(
+                labelText: "اسم المخيم",
+              ),
             ),
-          ),
-        
-
+            TextField(
+              readOnly: (widget.campsInfo == null) ? false : true,
+              controller: campCode,
+              decoration: InputDecoration(
+                labelText: "كود المخيم",
+              ),
+            ),
+            TextField(
+              controller: location,
+              decoration: InputDecoration(
+                labelText: "الموقع",
+              ),
+            ),
+            TextField(
+              keyboardType: TextInputType.number,
+              controller: tentsCount,
+              decoration: InputDecoration(
+                labelText: "عدد الخيم",
+              ),
+            ),
+            TextField(
+              keyboardType: TextInputType.number,
+              controller: familyCounts,
+              decoration: InputDecoration(
+                labelText: "عدد العائلات",
+              ),
+            ),
+            TextField(
+              controller: needs,
+              decoration: InputDecoration(
+                labelText: "عدد الأشخاص",
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                (widget.campsInfo == null)
+                    ? Column(
+                        children: <Widget>[
+//                          GoogleMap(
+//                            onMapCreated: _onMapCreated,
+//                            initialCameraPosition: CameraPosition(
+//                              target: LatLng (0,0),
+//                              zoom: 11.0,
+//                            ),
+//                          ),
+                          RaisedButton(
+                            child: Text("اضافة المخيم"),
+                            onPressed: () {
+                              fireStorage.addCamps(CampsInfo(
+                                  campName: campName.text,
+                                  campCode: campCode.text,
+                                  location: location.text,
+                                  needs: needs.text,
+                                  tentsCount: int.parse(tentsCount.text),
+                                  familyCounts: int.parse(familyCounts.text)));
+                              Navigator.pop(context);
+                            },
+                          ),
+                        ],
+                      )
+                    : Column(
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.only(left: 0.0, top: 10.0, right: 0.0, bottom: 0.0),
+                          child: Center(
+                            child: Image.asset(
+                              'assets/imgs/map.jpg',
+                              height: 214,
+                            ),
+                          ),
+                        ),
+                        RaisedButton(
+                            child: Text("تعديل معلومات المخيم"),
+                            onPressed: () {
+                              fireStorage.updateCamps(CampsInfo(
+                                  campName: campName.text,
+                                  campCode: campCode.text,
+                                  location: location.text,
+                                  needs: needs.text,
+                                  tentsCount: int.parse(tentsCount.text),
+                                  familyCounts: int.parse(familyCounts.text)));
+                              Navigator.pop(context);
+                            },
+                          ),
+                      ],
+                    ),
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:unhcr_camps/Models/Camps.dart';
 import 'package:unhcr_camps/Services/firebase.dart';
 import 'package:unhcr_camps/addCamps.dart';
 
-
-void main(){
+void main() {
   runApp(new MaterialApp(
-    title: "Camps",
+    title: "Sawa",
     home: new Camps(),
   ));
 }
@@ -18,56 +18,192 @@ class Camps extends StatefulWidget {
 }
 
 class _CampsState extends State<Camps> {
-  FireStorage fireStorage=new FireStorage();
+  FireStorage fireStorage = new FireStorage();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Camps"),
-        centerTitle: true,
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.add),
-            onPressed: ()=>
-            Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => addCamps()),
-            )
+        floatingActionButton: FloatingActionButton(
+            child: Text("إضافة"),
+            onPressed: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => addCamps()),
+                )),
+        body: _buildBody(context)
+        // body: ,
+        );
+  }
 
-          )
+  Widget _buildBody(BuildContext context) {
+    return Stack(
+      children: <Widget>[_buildBackground(), _buildBodyContent()],
+    );
+  }
 
+  Widget _buildListView(BuildContext context) {
+    return Container(
+      child: FutureBuilder(
+          future: fireStorage.getCamps(),
+          builder:
+              (BuildContext context, AsyncSnapshot<List<CampsInfo>> snapshot) {
+            if (snapshot.hasData && snapshot.data.isNotEmpty)
+              return Container(
+                child: ListView.builder(
+                    itemCount: snapshot.data.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      final item = snapshot.data[index];
+                      return Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: _getListItemUI(item, index),
+//                        child: ListTile(
+//                          onTap: () => Navigator.push(
+//                            context,
+//                            MaterialPageRoute(
+//                                builder: (context) => addCamps(
+//                                      campsInfo: item,
+//                                    )),
+//                          ),
+//                          title: Text(snapshot.data[index].campName),
+//                          subtitle: Text(snapshot.data[index].location),
+//                        ),
+                      );
+                    }),
+              );
+            else
+              return Container(
+                child: Center(
+                  child: Text("لا يوجد مخيمات"),
+                ),
+              );
+          }),
+    );
+  }
+
+  Widget _buildBackground() {
+    return Container(
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+              colors: [Colors.white, Colors.white],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomLeft)),
+    );
+  }
+
+  Widget _buildBodyContent() {
+    return SafeArea(
+      child: Column(
+        children: <Widget>[
+          _buildIcon(),
+          Expanded(child: _buildListView(context))
         ],
       ),
-body: Material(child: FutureBuilder(
-  future: fireStorage.getCamps(),
-    builder: (BuildContext context,
-    AsyncSnapshot<List<CampsInfo>> snapshot){
-    if(snapshot.hasData&&snapshot.data.isNotEmpty)
-    return ListView.builder(
-          itemCount: snapshot.data.length,
-    itemBuilder: (BuildContext context, int index) {
-      final item = snapshot.data[index];
-      return ListTile(
-        onTap: ()=>
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => addCamps(campsInfo: item,)),
-          ),
-        title: Text(snapshot.data[index].campName),
-        subtitle: Text(snapshot.data[index].location),
-      );
-    }
-        );
+    );
+  }
 
-else return Container(
-      child: Center(
-        child: Text("Now Data"),
+  Widget _buildIcon() {
+    double padding = 10.0;
+//    if (kIsWeb) {
+//      padding = 20.0;
+//    }
+
+    return Padding(
+      padding: EdgeInsets.all(padding),
+      child: Column(
+        children: <Widget>[
+          Center(
+            child: Image.asset(
+              'assets/imgs/sawa.png',
+              width: 200,
+              height: 100,
+            ),
+          ),
+          new Container(
+              padding: const EdgeInsets.only(left: 12.0, right: 1.0),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: new Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        new TextField(
+                          decoration: new InputDecoration(
+                              labelText: "أدخل اسم المخيم للبحث"),
+                          keyboardType: TextInputType.number,
+                          inputFormatters: <TextInputFormatter>[
+                            WhitelistingTextInputFormatter.digitsOnly
+                          ], // Only numbers can be entered
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(1.0),
+                    child: Card(
+                        color: Colors.lightBlueAccent,
+                        child: Padding(
+                          padding: const EdgeInsets.only(
+                              left: 20.0, right: 20.0, top: 10.0, bottom: 10.0),
+                          child: Text(
+                            "ابحث",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        )),
+                  )
+                ],
+              )),
+        ],
       ),
     );
+  }
 
-    }),
+  Widget _getListItemUI(CampsInfo campsInfo, int index) {
+    double elevationValue = 12.0;
+//    if (kIsWeb) {
+//      elevationValue = 2.0;
+//    }
 
-),// body: ,
-    );
+    return new Card(
+        elevation: elevationValue,
+        color: Colors.white,
+        child: new Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: new ListTile(
+                leading: new Image.asset(
+                  "assets/imgs/" +index.toString()+"a.png",
+                  fit: BoxFit.contain,
+                  width: 60,
+                  height: 100,
+                ),
+                title: new Text(
+                  campsInfo.campName,
+                  style: new TextStyle(
+                      fontSize: 14.0, fontWeight: FontWeight.bold),
+                ),
+                subtitle: Expanded(
+                  child: new Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        new Text(campsInfo.location,
+                            style: new TextStyle(
+                                fontSize: 13.0, fontWeight: FontWeight.normal)),
+                        new Text(' عدد العوائل : ${campsInfo.tentsCount}',
+                            style: new TextStyle(
+                                fontSize: 11.0, fontWeight: FontWeight.normal)),
+                      ]),
+                ),
+                onTap: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => addCamps(
+                            campsInfo: campsInfo,
+                          )),
+                ),
+              ),
+            )
+          ],
+        ));
   }
 }
